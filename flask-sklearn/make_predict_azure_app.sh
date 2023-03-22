9e1a105ed59f6babb52fceb9c306edc3bf989b67
Wojciech Siwecki <Wojciech.Siwecki@arvato-scs.com>
2023-03-22 12:58:51 +0100

    comment from notebook

--- flask-sklearn/make_predict_azure_app.sh
+++ flask-sklearn/make_predict_azure_app.sh
@@ -26,4 +26,5 @@ curl -d '{
 }'\
      -H "Content-Type: application/json" \
      -X POST https://flaskws.azurewebsites.net:$PORT/predict 
+     #add comment on notebook
      #your application name <yourappname>goes here
